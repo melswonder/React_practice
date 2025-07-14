e32bfd5decb8353210a0826a35763b4b9d000844
mels wonder <melswonder@melsnoMacBook-Air.local>
2025-07-14 13:16:31 +0900

    add ex01 fix ex00

---
+++ ex01/src/App.js
@@ -0,0 +1,12 @@
+import Profile from "./Profile/Profile.js";
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
